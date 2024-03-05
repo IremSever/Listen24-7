2aad58c59ea587f25c5f46afc77e8767ad34ca4f
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-03-05 21:26:47 +0300

    up

---
+++ Listen24-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Listen24-7
+//
+//  Created by İrem Sever on 5.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
