66558629e08bdc0345434e694a4369a38aa2bad3
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-03-08 11:18:49 +0300

    up

--- Listen24-7/Home/Cells/Top10/CollectionViewCell/Top10CollectionViewCell.swift
+++ Listen24-7/Home/Cells/Top10/CollectionViewCell/Top10CollectionViewCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class Top10CollectionViewCell: UICollectionViewCell {
 
+    
+    @IBOutlet weak var lblSingerName2: UILabel!
+    @IBOutlet weak var lblSonName2: UILabel!
+    @IBOutlet weak var lblListNo2: UILabel!
+    @IBOutlet weak var lblSingerName: UILabel!
+    @IBOutlet weak var lblSongName: UILabel!
+    @IBOutlet weak var lblListNo: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
