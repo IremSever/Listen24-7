00fdc925755df91aa6ae4fb08968b527c1e5a643
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-03-18 14:52:03 +0300

    UP

--- Listen24-7/Home/Cells/Latest/CollectionViewCell/LatestCollectionViewCell.swift
+++ Listen24-7/Home/Cells/Latest/CollectionViewCell/LatestCollectionViewCell.swift
@@ -26,11 +26,11 @@ class LatestCollectionViewCell: UICollectionViewCell {
         }
         
         lblTitle.text = data.title
-        lblTitle.font = UIFont(name: "Futura-Bold", size: 13)
+        lblTitle.font = UIFont(name: "Futura-Bold", size: 10)
         lblTitle.textColor = UIColor.black
         
-        lblMinutes.text = data.title
-        lblMinutes.font = UIFont(name: "Futura", size: 10)
+        lblMinutes.text = data.duration
+        lblMinutes.font = UIFont(name: "Futura", size: 8)
         lblMinutes.textColor = UIColor.black
     }
 }
