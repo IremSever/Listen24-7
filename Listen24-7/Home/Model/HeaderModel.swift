2feb4c548a1f6b013bbfd42f6618c5ae1d19e443
Irem Sever <99093113+IremSever@users.noreply.github.com>
2024-03-25 09:55:27 +0300

    up

--- Listen24-7/Home/Model/HeaderModel.swift
+++ Listen24-7/Home/Model/HeaderModel.swift
@@ -6,28 +6,33 @@
 //
 
 import Foundation
+
 // MARK: - HeaderModel
 struct HeaderModel: Codable {
     let meta: HeaderMeta
     let data: HeaderData
 }
-// MARK: - DataClass
+
+// MARK: - HeaderData
 struct HeaderData: Codable {
     let news: News
 }
+
 // MARK: - News
 struct News: Codable {
     let pageInfo: JSONHeaderNull?
-    let response: [Response]
+    let response: [HeaderResponse]?
     let status: Bool
+
     enum CodingKeys: String, CodingKey {
         case pageInfo = "PageInfo"
         case response = "Response"
         case status = "Status"
     }
 }
+
 // MARK: - Response
-struct Response: Codable {
+struct HeaderResponse: Codable {
     let articleID, description, external, id: String
     let image: String
     let imageAlternateText, modifiedDate: String
@@ -37,12 +42,14 @@ struct Response: Codable {
     let title, titleShort: String
     let url: String
     let usedMethod: Bool
+
     enum CodingKeys: String, CodingKey {
         case articleID = "ArticleId"
         case description = "Description"
         case external = "External"
         case id = "Id"
-        case image, imageAlternateText
+        case image = "Image"
+        case imageAlternateText = "ImageAlternateText"
         case modifiedDate = "ModifiedDate"
         case richTextActive = "RichTextActive"
         case showWebView = "ShowWebView"
@@ -56,31 +63,39 @@ struct Response: Codable {
         case usedMethod = "UsedMethod"
     }
 }
+
 // MARK: - Meta
 struct HeaderMeta: Codable {
     let statusCode: Int
     let message, description, brand: String
+
     enum CodingKeys: String, CodingKey {
         case statusCode = "status_code"
         case message, description, brand
     }
 }
+
+// MARK: - Encode/decode helpers
 // MARK: - Encode/decode helpers
 class JSONHeaderNull: Codable, Hashable {
-    public static func == (lhs: JSONHeaderNull, rhs: JSONHeaderNull) -> Bool {
+    static func == (lhs: JSONHeaderNull, rhs: JSONHeaderNull) -> Bool {
         return true
     }
-    public var hashValue: Int {
-        return 0
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(0)
     }
-    public init() {}
-    public required init(from decoder: Decoder) throws {
+    
+    init() {}
+    
+    required init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         if !container.decodeNil() {
             throw DecodingError.typeMismatch(JSONHeaderNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
         }
     }
-    public func encode(to encoder: Encoder) throws {
+    
+    func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encodeNil()
     }
