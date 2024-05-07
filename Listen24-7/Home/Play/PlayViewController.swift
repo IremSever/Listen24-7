f5f89d7e61db6a5ccc7a1466153f0408b52206e6
İrem Sever <iremsever_@outlook.com>
2024-05-07 15:08:41 +0300

    up

--- Listen24-7/Home/Play/PlayViewController.swift
+++ Listen24-7/Home/Play/PlayViewController.swift
@@ -9,11 +9,10 @@ import Foundation
 import UIKit
 class PlayViewController: UIViewController {
     
-    
-    @IBOutlet weak var tableViewPlay: UITableView!
-    @IBOutlet weak var buttonBack: UIButton!
     static let identifier = "PlayViewController"
     
+    @IBOutlet weak var buttonBack: UIButton!
+    @IBOutlet weak var imgBlack: UIImageView!
     @IBOutlet weak var lblTimeElapsed: UILabel!
     @IBOutlet weak var lblTimeTotal: UILabel!
     @IBOutlet weak var buttonBackward: UIButton!
@@ -33,30 +32,24 @@ class PlayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        buttonBack.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        
+        buttonBack?.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        if let selectedIndex = selectedIndex, let selectedSong = listForPlayer.first?.songs?[selectedIndex] {
+            configure(with: selectedSong)
+            
+            
+        }
         print("items: ", selectedIndex , listForPlayer)
         print("mp3 url: ", listForPlayer.first?.songs?[selectedIndex ?? 0].mp3URL)
+        
+        styleController()
     }
     
     @objc func backButtonTapped() {
         navigationController?.popViewController(animated: true)
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        lblName?.font = UIFont(name: "Futura-Bold", size: 13)
-        lblName?.textColor = UIColor.systemPurple
-        
-        lblArtist?.font = UIFont(name: "Futura", size: 10)
-        lblArtist?.textColor = UIColor.gray
-        
-        
-    }
-    func configure(with data: PlaylistSongs?) {
-        guard let playlist = data?.playlists?.first,
-              let imageURLString = playlist.image,
+    func configure(with data: PlaylistSongs) {
+        guard let imageURLString = data.playlists?.first?.image,
               let imageURL = URL(string: imageURLString) else {
             imgSong?.image = nil
             lblName.text = nil
@@ -66,7 +59,9 @@ class PlayViewController: UIViewController {
             return
         }
         
-        lblTimeTotal.text = data?.durationTime
+        lblName.text = data.name
+        lblArtist.text = data.singers?.first?.name
+        lblTimeTotal.text = data.durationTime
         lblTimeElapsed.text = "00:00"
         
         URLSession.shared.dataTask(with: imageURL) { [weak self] (data, response, error) in
@@ -80,7 +75,25 @@ class PlayViewController: UIViewController {
             }
         }.resume()
     }
-
+    
+    func styleController() {
+        lblName?.font = UIFont(name: "Futura-Bold", size: 15)
+        lblName?.textColor = UIColor.systemPurple
+        
+        lblArtist?.font = UIFont(name: "Futura", size: 12)
+        lblArtist?.textColor = UIColor.gray
+        
+        lblTimeElapsed?.font = UIFont(name: "Futura", size: 10)
+        lblTimeElapsed?.textColor = UIColor.gray
+        
+        lblTimeTotal?.font = UIFont(name: "Futura", size: 10)
+        lblTimeTotal?.textColor = UIColor.gray
+        
+        imgSong.layer.cornerRadius = 20
+        imgBlack.layer.cornerRadius = 20
+    }
 }
-  
+
+
+
 
