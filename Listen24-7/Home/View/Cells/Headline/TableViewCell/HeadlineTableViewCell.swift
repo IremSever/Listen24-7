f191e83bf831cc5a309eb48816695903707c4ed8
İrem Sever <iremsever_@outlook.com>
2024-05-10 11:18:47 +0300

    up

--- Listen24-7/Home/View/Cells/Headline/TableViewCell/HeadlineTableViewCell.swift
+++ Listen24-7/Home/View/Cells/Headline/TableViewCell/HeadlineTableViewCell.swift
@@ -11,24 +11,27 @@ protocol headlineCellProtocol {
     func didSelectedHeadline(with id: String)
 }
 
-class HeadlineTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate  {
+class HeadlineTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UIScrollViewDelegate {
+    
     @IBOutlet weak var pageControlHeadline: UIPageControl!
     @IBOutlet weak var collectionViewHeadline: UICollectionView!
     static let identifier = "HeadlineTableViewCell"
     var dataArray: [News] = [] {
         didSet {
             collectionViewHeadline.reloadData()
+            pageControlHeadline.numberOfPages = dataArray.first?.response?.count ?? 0
         }
     }
-    var selectedIndex = 0
     var selectedPlaylistId: Int?
-    
     var delegate: headlineCellProtocol?
+    var frameValue: CGFloat = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
         createHeadlineCollectionView()
+        pageControlHeadline.numberOfPages = dataArray.first?.response?.count ?? 0
     }
+    
     
     func createHeadlineCollectionView() {
         let layout = UICollectionViewFlowLayout()
@@ -41,30 +44,35 @@ class HeadlineTableViewCell: UITableViewCell, UICollectionViewDataSource, UIColl
         collectionViewHeadline.dataSource = self
         collectionViewHeadline.showsHorizontalScrollIndicator = false
         collectionViewHeadline.isPagingEnabled = true
-        
-        let screenWidth = UIScreen.main.bounds.width
-        let cellWidth = screenWidth - 100
-        let horizontalInset = (screenWidth - cellWidth) / 2
-        collectionViewHeadline.contentInset = UIEdgeInsets(top: 0, left: horizontalInset, bottom: 0, right: horizontalInset)
     }
     
     func updateDataArray(with dataArray: [News]) {
         self.dataArray = dataArray
         collectionViewHeadline.reloadData()
+        pageControlHeadline.numberOfPages = dataArray.first?.response?.count ?? 0
         if dataArray.first?.response?.count ?? 0 > 0 {
-            let selectedIndexIndexPath = IndexPath(row: selectedIndex + ((dataArray.first?.response?.count ?? 0) * 500), section: 0)
-            collectionViewHeadline.scrollToItem(at: selectedIndexIndexPath, at: .centeredHorizontally, animated: false)
+            let selectedIndexPath = IndexPath(row: selectedPlaylistId ?? 0, section: 0)
+            collectionViewHeadline.scrollToItem(at: selectedIndexPath, at: .centeredHorizontally, animated: false)
         }
     }
     
+    /*func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+     let itemWidth = collectionViewHeadline.frame.width
+     let currentIndex = Int(collectionViewHeadline.contentOffset.x / itemWidth)
+     pageControlHeadline.currentPage = currentIndex
+     selectedIndex = currentIndex
+     
+     }*/
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return (dataArray.first?.response?.count ?? 0) * 1000
+        return dataArray.first?.response?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HeadlineCollectionViewCell.identifier, for: indexPath) as! HeadlineCollectionViewCell
-        let dataIndex = indexPath.item % (dataArray.first?.response?.count ?? 1)
-        if let item = dataArray.first?.response?[dataIndex] {
+        let currentIndex = indexPath.row
+        let totalItems = dataArray.first?.response?.count ?? 0
+        if let item = dataArray.first?.response?[currentIndex % totalItems] {
             cell.configure(with: item)
             
             if let playlistId = item.id {
@@ -73,6 +81,20 @@ class HeadlineTableViewCell: UITableViewCell, UICollectionViewDataSource, UIColl
                 self.selectedPlaylistId = nil
             }
         }
+        
+        let previousIndex = (currentIndex + totalItems - 1) % totalItems
+        let previousCell = collectionView.dequeueReusableCell(withReuseIdentifier: HeadlineCollectionViewCell.identifier, for: IndexPath(item: previousIndex, section: 0)) as! HeadlineCollectionViewCell
+        if let item = dataArray.first?.response?[previousIndex] {
+            previousCell.configure(with: item)
+        }
+        
+        // Sonraki hücre
+        let nextIndex = (currentIndex + 1) % totalItems
+        let nextCell = collectionView.dequeueReusableCell(withReuseIdentifier: HeadlineCollectionViewCell.identifier, for: IndexPath(item: nextIndex, section: 0)) as! HeadlineCollectionViewCell
+        if let item = dataArray.first?.response?[nextIndex] {
+            nextCell.configure(with: item)
+        }
+        
         return cell
     }
     
@@ -83,12 +105,23 @@ class HeadlineTableViewCell: UITableViewCell, UICollectionViewDataSource, UIColl
         delegate?.didSelectedHeadline(with: selectedPlaylistId)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width, height: 490)
+    }
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let screenWidth = UIScreen.main.bounds.width
-        let itemWidth = screenWidth - 100
-        let itemCount = dataArray.first?.response?.count ?? 1
-        let currentIndex = Int(collectionViewHeadline.contentOffset.x / itemWidth) % itemCount
-        selectedIndex = currentIndex
+        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
+        pageControlHeadline.currentPage = Int(pageNumber)
+        if pageControlHeadline.currentPage == 1 {
+            frameValue = pageNumber
+        }
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if pageControlHeadline.currentPage == dataArray.first?.response?.count ?? 0 - 1{
+            pageControlHeadline.currentPage = 0
+            collectionViewHeadline.contentOffset = CGPoint(x: 0, y: 0)
+        }
     }
 }
+
 
